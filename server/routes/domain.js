d87c38fd6014a2a3c7e553d00a54b246085d3bbc
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-05-29 14:34:23 -0300

    Generates a working endpoint
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- server/routes/domain.js
+++ server/routes/domain.js
@@ -2,25 +2,11 @@ const express = require('express')
 
 const app = express.Router()
 const {
-    createStory,
-    getAllStories,
-    updateStory,
-    deleteStory,
-    generateStoryFile
-} = require('../controllers/storyController.js')
-
+    generateDomainFile
+} = require('../controllers/domainController.js')
 
 app
-    .route('/:projectName/story')
-    .post(createStory)
-    .get(getAllStories)
-app
-    .route('/story/:storyId')
-    .put(updateStory)
-    .delete(deleteStory)
-
-app
-    .route('/:projectName/story/generate_file')
-    .get(generateStoryFile)
+    .route('/:projectName/domain/generate_file')
+    .get(generateDomainFile)
 
 module.exports = app
