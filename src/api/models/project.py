ffcb7cd5a7419132dc25d6db004adfcca21846e7
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2019-09-03 16:41:49 -0300

    Fix invalid reference

--- src/api/models/project.py
+++ src/api/models/project.py
@@ -1,17 +1,8 @@
 from djongo import models
 from boogie.rest import rest_api
-from .utter import Utter
-from .story import Story
-from .intent import Intent
 
 class Project(models.Model):
     name = models.TextField()
     description = models.TextField()
 
     objects = models.DjongoManager()
-
-    class Meta:
-        model = Blog
-        fields = (
-            'name'
-        )
