5588ea601c4252dabd0ffa6eaf30665f8b237f25
Victor Moura <victor_cmoura@hotmail.com>
2019-09-12 11:05:29 -0300

    Adds example field to each content in stories attr

--- src/api/models/story.py
+++ src/api/models/story.py
@@ -1,6 +1,9 @@
 from djongo import models
 from .project import Project
+from .intent import Intent
+from .utter import Utter
 from rest_framework import serializers
+import random
 
 class Story(models.Model):
     name = models.TextField(default="")
@@ -14,7 +17,27 @@ class Story(models.Model):
 class StorySerializer(serializers.ModelSerializer):
     content = serializers.SerializerMethodField()
 
+    def get_example(self, obj):
+        intent_ids = [element['id'] for element in obj.content if element['type'] == 'intent']
+        intents = Intent.objects.filter(pk__in=intent_ids)
+
+        utter_ids = [element['id'] for element in obj.content if element['type'] == 'utter']
+        utters = Utter.objects.filter(pk__in=utter_ids)
+
+        elements = {}
+        elements['intent'] = intents
+        elements['utter'] = utters
+
+        field = {}
+        field['intent'] = 'samples'
+        field['utter'] = 'alternatives'
+
+        for i, element in enumerate(obj.content):
+            element_obj = elements[element['type']].filter(pk=element['id']).first()
+            obj.content[i]['example'] = random.choice(getattr(element_obj, field[element['type']]))
+
     def get_content(self, obj):
+        self.get_example(obj)
         return [dict(content) for content in obj.content]
 
     class Meta:
