77c8f0999b2202446d6baba02d0297a9cbd1bd35
Victor Moura <victor_cmoura@hotmail.com>
2019-09-26 10:08:56 -0300

    Adds story cascade delete on intent and utter delete operations

--- src/api/utils/__init__.py
+++ src/api/utils/__init__.py
@@ -1,5 +1,5 @@
 from .formatters import request_to_dict, story_content_formatter
-from .story_validation import validate_content, validate_story
+from .story_validation import validate_content, validate_story, delete_related_stories
 from .intent_validation import validate_intent
 from .utter_validation import validate_utter
 from .story_filters import filter_content_by_name
