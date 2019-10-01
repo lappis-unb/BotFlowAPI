1ae876e0f2465e90195651159660af0514dae5eb
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2019-10-01 10:12:18 -0300

    Add first test

--- src/api/tests/test_parser.py
+++ src/api/tests/test_parser.py
@@ -6,22 +6,21 @@ from api.utils import story_content_formatter
 
 class TestDomainParser:
 
-    @pytest.fixture
-    def project_1(self, db):
-        """
-        Project with one story and without entities.
-        """
-        project = Project.objects.create(name='name', description='description')
-        Intent.objects.create(name='cumprimentar', project=project, samples=['oi', 'ola', 'bom dia'])
-        Story.objects.create(name='Despedir Story', project=project, content=story_content_formatter([
-            {'name': 'cumprimentar', 'type': 'intent'},
-            {'name': 'utter_cumprimentar', 'type': 'utter'}
+    #@pytest.fixture
+    #def project_1(self, db):
+    #   """
+    #    Project with one story and without entities.
+    #    """
+    #    project = Project.objects.create(name='name', description='description')
+    #    Intent.objects.create(name='cumprimentar', project=project, samples=['oi', 'ola', 'bom dia'])
+    #    Story.objects.create(name='Despedir Story', project=project, content=story_content_formatter([
+    #        {'name': 'cumprimentar', 'type': 'intent'},
+    #        {'name': 'utter_cumprimentar', 'type': 'utter'}
 
-        ]))
+    #   ]))
 
-        return project
+    #    return project
 
-    def test_domain_without_entities(self, project_1):
-        parser = DomainParser()
-        print(parser.parse(project_1))
+    def test_domain_without_entities(self, db):
+        print(Project.objects.all())
         assert False
