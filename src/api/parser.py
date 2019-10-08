41a2d129f40ba5ca69c26bc9afcf84124d7ec963
GeovanaRamos <geovannna.1998@gmail.com>
2019-10-08 09:25:04 -0300

    Added checkpoint parsing

--- src/api/parser.py
+++ src/api/parser.py
@@ -8,6 +8,9 @@ def format_utter(utter_name):
     else:
         return utter_name
 
+def format_checkpoint(name):
+    return "checkpoint_" + name
+
 class StoryParser:
     """
     Generate a markdown string from a given
@@ -16,12 +19,17 @@ class StoryParser:
     def parse(self, story: Story):
         name = f'## {unidecode(story.name)}\n'
         body = ''
-        
+
+        if story.is_checkpoint:
+            body += self._checkpoint_parser(story.name)
+
         for c in story.content:
             if c['type'] == "intent":
                 body += self._intent_parser(c)
             elif c['type'] == "utter":
                 body += '\t' + self._utter_parser(c)
+            elif c['type'] == "checkpoint":
+                body += self._checkpoint_parser(c["name"])
 
         return name + body + '\n'
 
@@ -29,9 +37,11 @@ class StoryParser:
     def _intent_parser(self, intent):
         return f'* {unidecode(intent["name"])}\n'
 
-    
     def _utter_parser(self, utter):
         return f'- {unidecode(format_utter(utter["name"]))}\n'
+
+    def _checkpoint_parser(self, name):
+        return f'> {unidecode(format_checkpoint(name))}\n'
 
 
 class IntentParser:
@@ -55,7 +65,7 @@ class DomainParser:
     """
     def parse(self, project: Project):
         content = ''
-        
+
         intents = Intent.objects.filter(project=project)
         utters = Utter.objects.filter(project=project)
         entities = []
@@ -67,7 +77,7 @@ class DomainParser:
        # content += self._generic_list_parser('entities', [e.name for e in entities])
         content += self._templates_parser(utters)
         content += self._generic_list_parser('actions', [unidecode(format_utter(u.name)) for u in utters])
-   
+
         return content
 
     def _generic_list_parser(self, name: str, elements: list):
@@ -75,17 +85,17 @@ class DomainParser:
 
         for e in elements:
             result += f'  - {e}\n'
-   
+
         return result + '\n'
-    
-    
+
+
 
 
 
     def _templates_parser(self, utters: list):
         result = f'templates:\n'
 
-        for u in utters:    
+        for u in utters:
             ident = 2 * ' '
             utter_name = format_utter(u.name)
             result += f'{ident}{unidecode(utter_name)}:\n'
@@ -96,5 +106,5 @@ class DomainParser:
                     ident = 10 * ' '
                     result += f'{ident}{t}\n'
                     result += f'\n'
-        
+
         return result + '\n'
