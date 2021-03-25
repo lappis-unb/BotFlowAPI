e248fb845e9fd59696b85cd38fa1eb8d0e67d544
Leticia Araujo <leticiards.04@gmail.com>
2021-03-24 22:41:02 -0300

    [#156] - Adding final unit tests

--- src/tests/files_tests.py
+++ src/tests/files_tests.py
@@ -3,45 +3,49 @@ from django.urls import reverse, resolve
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
-from api.models import project, story, intent
-from .views import story
+from api.models import projectModel, storyModel, intentModel, utterModel
+from api.views import utterView, projectView, intentView
 
-class StoriesFileTest(TestCase):
-    def setUp(self):
-        self.story = Story.objects.create(id = '10', name = 'teste', content = [
-            {
-                "name": "content name",
-                "type": "intent"
-            },
-            {
-                "name": "content name 2",
-                "type": "utter"
-            },
-            {
-                "name": "content name 3",
-                "type": "utter"
-            }
-        ])
-        self.factory = APIRequestFactory()
-        self.view = StoriesFile.as_view()
-        self.client = APIClient()
-        self.data = {'id':'10', 'name':'teste', 'content': [
-            {
-                "name": "content name",
-                "type": "intent"
-            },
-            {
-                "name": "content name",
-                "type": "utter"
-            },
-            {
-                "name": "content name",
-                "type": "utter"
-            }
-        ]}
+class StoriesTest(TestCase):
+    
+    def testModelStory(self):
+        storyModel.objects.create(
+            id = 50, name = 'Teste', content = [
+                {name: 'TesteContent',
+                 type: 'utter'}]
+        )
 
-    def test_story_view_success_status_code(self):
-        url = reverse('api:story', kwargs={'pk': self.story.pk})
-        request = self.factory.get(url)
-        response = self.view(request, pk=self.story.pk)
-        self.assertEquals(response.status_code, 200)
+    def testModelStoryGet(self):
+        idStory = storyModel.objects.get(id = 50)
+        self.assertEqual(storyModel.object.get_content, 50)
+
+class UtterTest(TestCase):
+    def testModelUtter(self):
+        utterModel.objects.create(
+            id = 49, name = 'Teste'
+        )
+
+    def testModelUtterGet(self):
+        idUtter = utterModel.objects.get(id = 49)
+        self.assertEqual(utter.object.utterView.get, 49)
+
+class ProjectTest(TestCase):
+    def testModelProject(self):
+        projectModel.objects.create(
+            id = 3, name = 'Teste', description = "This is a Rasa bot!"
+        )
+
+    def testModelProjectGet(self):
+        idProject= projectModel.objects.get(id = 3)
+        self.assertEqual(utter.object.projectView.get, 3)
+
+class IntentTest(TestCase):
+    def testModelIntent(self):
+        intentModel.objects.create(
+            id = 52, name = 'Teste'
+        )
+
+    def testModelIntentGet(self):
+        idIntent = intentModel.objects.get(id = 3)
+        self.assertEqual(utter.object.intentView.get, 3)
+
